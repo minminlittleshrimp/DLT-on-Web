2c59a7607f4e2af629f6fd9deb43fc9b73408f30
minminlittleahrimp <minhmark47@gmail.com>
2021-11-03 18:05:46 +0700

    modifed 2

--- openfile.js
+++ openfile.js
@@ -13,12 +13,22 @@ switcher.addEventListener('click', function(){
     console.log('current class name: '+className);
 });
 
-function myFunction(){
-    const inpObj=document.getElementById("id1");
-    if(!inpObj.checkValidity()){
-        document.getElementById("msg").innerHTML="Please choose file";
-    }
-    else{
-        document.getElementById("msg").innerHTML="Input is accepted";
-    }
+function showLog(){
+    document.getElementById('files')
 }
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
