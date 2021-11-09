7a213f30cbe7f151d3320273904e71b8b35b1a00
minminlittleahrimp <minhmark47@gmail.com>
2021-11-09 16:53:50 +0700

    update openjs 2

--- openfile.js
+++ openfile.js
@@ -3,6 +3,10 @@ function toHexString(byteArray) {
     (output + ('0' + elem.toString(16)).slice(-2)),'');
 };
 
+function chunkString(str, length) {
+  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+};
+
 function detectPattern(text){
   //create pattern DLT0x01
   var str = toHexString(text);
@@ -16,41 +20,38 @@ function epoch2BrokenTime(){
 
 };
 
-function previewFile() {
+function previewFile() 
+{
   const input = document.querySelector('input');
   const reader = new FileReader();
   const fileByteArray = [];
   hex4Bytes = [];
 
-  input.addEventListener('change', (e) => {
-    reader.readAsArrayBuffer(e.target.files[0]);
-    reader.onload = (evt) => {
-      
-      if (evt.target.readyState === FileReader.DONE) {
-        const arrayBuffer = evt.target.result;
+  input.addEventListener
+  ('change', (e) => 
+    {
+      reader.readAsArrayBuffer(e.target.files[0]);
+      reader.onload = (evt) => 
+      {
+        if (evt.target.readyState === FileReader.DONE) 
+        {
+          const arrayBuffer = evt.target.result;
         
-        array = new Uint8Array(arrayBuffer);
-        hex1Byte = toHexString(array);
-        var countByte = 0;
-        for (const a of hex1Byte) {
-          if (countByte < 4){
-            hex4Bytes.push(a);
-            countByte = countByte+1;
-          }
-          else{
-            fileByteArray.push(hex4Bytes);
-            hex4Bytes = [];
-            countByte = 0;
-          }
+          array = new Uint8Array(arrayBuffer);
+        
+          for (const a of array)
+          {
+            fileByteArray.push(a);
+          };
+          hexFile = toHexString(fileByteArray);
+
+          //console.log(hexFile);  
+
+          hex4Bytes = chunkString(hexFile, 4);
           
-          /*if(detectPattern(a)){
-            epoch2BrokenTime()
-          }  */      
-          
-          
+
         }
-        console.log(fileByteArray);
       }
     }
-  })
+  )
 };
