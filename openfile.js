c77cb17f195327fe951ea27424b7959b41b5071e
minminlittleahrimp <minhmark47@gmail.com>
2021-11-11 11:31:56 +0700

    newVer1.0.4

--- openfile.js
+++ openfile.js
@@ -1,57 +1,30 @@
+//convert from binary to hex
 function toHexString(byteArray) {
   return byteArray.reduce((output, elem) => 
     (output + ('0' + elem.toString(16)).slice(-2)),'');
 };
 
-function chunkString(str, length) {
+//slice string to Byte element
+/*function chunkString(str, length) {
   return str.match(new RegExp('.{1,' + length + '}', 'g'));
-};
+};*/
 
-function detectPattern(chunkHex1Byte){
-  //find pattern DLT0x01 = 444c 5401
-  var index = 0;
-  chunkHex1Byte.forEach(element,index => {
-    if (element == 4){
-      index++;
-      if (element == 4){
-        index++; 
-        if (element == 4){
-          index++;
-          if (element == c){
-            index++;
-            if (element == 5){
-                index++;
-                if (element == 4){
-                  index++;
-                  if (element == 0){
-                    index++;
-                    if (element == 1){
-                      return 1;
-                    }
-                  }    
-                }
-              }
-            }
-          }
-        }
-      }     
-    }    
-  });  
+//detect pattern DLT0x01
+lineIndex = [];
+function detectPattern(strInput){
+  //define pattern DLT0x01 = 444c 5401 as regular expression
+  var patternDLT;
+  patternDLT = new RegExp('444c5401','g');
+  while (patternDLT.test(strInput)){
+    lineIndex.push(patternDLT.lastIndex);
+  }
 };
 
 function epoch2BrokenTime(){
 
 };
 
-function objToString(obj) {
-  var str = 'Object: ';
-  for (var p in obj) {
-    if (obj.hasOwnProperty(p)) {
-      str += p + '::' + obj[p] + ',\n';
-    }
-  }
-  return str;
-}
+
 
 
 function handleFileSelect() 
@@ -59,7 +32,8 @@ function handleFileSelect()
   const input = document.querySelector('input');
   const reader = new FileReader();
   const fileByteArray = [];
-  hex1Byte = [];
+  hex1Byte = '';
+  
 
   input.addEventListener
   ('change', (e) => 
@@ -77,29 +51,19 @@ function handleFileSelect()
           {
             fileByteArray.push(a);
           };
-          hexFile = toHexString(fileByteArray);
-          hex1Byte = chunkString(hexFile, 1);
-          document.getElementById('list').innerHTML = '<ul>' + hex1Byte + '</ul>';
-          indexArray = 0;
-          hex1Byte.forEach(element, indexArray =>{
+          hex1Byte = toHexString(fileByteArray);
+          //hexFile = toHexString(fileByteArray);
+          //hex1Byte = chunkString(hexFile, 1).join('');
+          detectPattern(hex1Byte);
+          document.getElementById('list').innerHTML = '<ul>' + lineIndex + '</ul>';
+          for (const i of lineIndex){
             
-          });
+          }
         } 
       }
     }
   )
-  //logThis(hex4Bytes);   
-  //document.getElementById('list').innerHTML = '<ul>' + fileByteArray.join('') + '</ul>';
 };
-(function () {
-  var old = console.log;
-  var logger = document.getElementById('log');
-  console.log = function (message) {
-      if (typeof message == 'object') {
-          logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
-      } else {
-          logger.innerHTML += message + '<br />';
-      }
-  }
-})();
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+
+document.getElementById('files').addEventListener('click', handleFileSelect, false);
