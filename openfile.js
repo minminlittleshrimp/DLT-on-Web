2d968a5d49d6d2becdca51dc9107e6efe00ac234
minminlittleahrimp <minhmark47@gmail.com>
2021-11-11 17:02:56 +0700

    done broken time

--- openfile.js
+++ openfile.js
@@ -20,10 +20,40 @@ function detectPattern(strInput){
   }
 };
 
-function epoch2BrokenTime(){
+/*function hex2Ascii(hex) {
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+      str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}*/
+
+
+function epoch2BrokenTime(str, index){
+  //Extract epoch second - second counted from 1.1.1970
+  epochStr = str.substring(index, index+8); 
+  const invertHex = [];
+  
+  for (let i=0; i<epochStr.length/2; i++){
+    invertHex.push(epochStr.substr(6-i*2, 2));
+  }
+
+  hexStr = invertHex.join('');
+  decimalStr = parseInt(hexStr, 16);
+
+  //Epoch to date
+  var dateTime = new Date(decimalStr*1000); 
+  dateTime = dateTime.toISOString(decimalStr);
+  
+  //Reformat
+  dateTime = dateTime.replace('-', '/').replace('-', '/').replace('T', ' ').replace('000Z', ' ');
+  //document.getElementById('list').innerHTML = '<ul>' + dateTime + '</ul>';
 
 };
 
+function epoch2Microsecond(str, index){
+  document.getElementById('list').innerHTML = '<ul>' + str + '</ul>';
+
+};
 
 
 
@@ -55,10 +85,12 @@ function handleFileSelect()
           //hexFile = toHexString(fileByteArray);
           //hex1Byte = chunkString(hexFile, 1).join('');
           detectPattern(hex1Byte);
-          document.getElementById('list').innerHTML = '<ul>' + lineIndex + '</ul>';
-          for (const i of lineIndex){
-            
-          }
+          //console.log();
+          //document.getElementById('list').innerHTML = '<ul>' + arrayBuffer + '</ul>';
+          //for (const i of lineIndex){
+            epoch2BrokenTime(hex1Byte, 8);
+            epoch2Microsecond(hex1Byte, 8+8);
+          //}
         } 
       }
     }
