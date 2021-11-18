d01ccc8835297e3bb972e3731e91af5ea250544f
Luu Quang Minh <Minh.LuuQuang@vn.bosch.com>
2021-11-18 09:43:03 +0700

    check connection and upload some logstorage config files
    Change-Id: I49f5a1f604c39d5a76c4b90fbd031a11c7ce6ad9

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('buttonjs');
-    button.addEventListener('click', function() {
-        //alert("here we are");
-        window.open("", "_blank");
-    });
-}, false);
---
+++ readme.txt
@@ -0,0 +1,15 @@
+TOKEN 	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
+
+Plugin UI:
+title: log update
+update button
+browse...
+log message data set-> table
+
+[FILTER1]
+LogAppName=LOG
+ContextName=TEST
+LogLevel=DLT_LOG_WARN
+File=example
+FileSize=50000
+NOFiles=5
